95b58543c0a49b0e3490280918af1c2f6a524cc6
melchisedech333 <melchisedech333@gmail.com>
2022-03-26 20:07:19 -0300

    Updates

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -1,5 +1,10 @@
-rm -rf code lex.yy.c y.tab.*
-bison -dy code.y
-lex code.l
-gcc -o code lex.yy.c y.tab.c
-./code
+echo "Compile: $1"
+rm -rf parsing y.tab.c y.tab.h lex.yy.c
+bison -dy "$1/code.y"
+mv y.tab.c "$1/y.tab.c"
+mv y.tab.h "$1/y.tab.h"
+lex "$1/code.l"
+mv lex.yy.c "$1/lex.yy.c"
+gcc -o parsing "$1/lex.yy.c" "$1/y.tab.c"
+
+
